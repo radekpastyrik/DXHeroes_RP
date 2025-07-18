ef6b9525573d5fd630ea57a52267f6e455307355
Radek_Pastyrik <radek.pastyrik@micronova.cz>
2025-07-18 21:50:54 +0200

    Fixed problems with tests

--- PythonSDK_offers/offers_sdk/__init__.py
+++ PythonSDK_offers/offers_sdk/__init__.py
@@ -2,15 +2,10 @@
 from .client import OffersClient
 from .models import Product, Offer
 from .exceptions import OffersAPIError
-from .retry import RetryConfig, AGGRESSIVE_RETRY, CONSERVATIVE_RETRY, FAST_RETRY
 
 __all__ = [
     "OffersClient", 
     "Product", 
     "Offer", 
-    "OffersAPIError",
-    "RetryConfig",
-    "AGGRESSIVE_RETRY",
-    "CONSERVATIVE_RETRY", 
-    "FAST_RETRY"
+    "OffersAPIError"
 ]
